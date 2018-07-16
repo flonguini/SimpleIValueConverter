7c70a6ca8bd347fd405e35ac7d0ea4936dcff673
Fernando Longuini <flonguini@outlook.com>
2018-07-16 03:52:36 -0300

    first commit

---
+++ SimpleIValueConverter/Classes/BackgroundConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace SimpleIValueConverter.Classes
+{
+    public class BackgroundConverter : System.Windows.Data.IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value != null)
+            {
+                if (value.ToString().ToLower() == "male")
+                {
+                    return new SolidColorBrush(new Color() { A = 255, R = 255, G = 0, B = 0 });
+                }
+                else
+                {
+                    return new SolidColorBrush(new Color() { A = 255, R = 0, G = 255, B = 255 });
+                }
+            }
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SimpleIValueConverter/Classes/Person.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace SimpleIValueConverter.Classes
+{
+    public class Person : INotifyPropertyChanged
+    {
+
+        private string _name;
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; OnPropertyChanged(); }
+        }
+        private int _age;
+
+        public int Age
+        {
+            get { return _age; }
+            set { _age = value; OnPropertyChanged(); }
+        }
+        private string _gender;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string Gender
+        {
+            get { return _gender; }
+            set { _gender = value; OnPropertyChanged(); }
+        }
+
+        private void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+    }
+}
